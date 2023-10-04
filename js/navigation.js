0e7ca9f06007d9258fb59f56c144165b56b6ab5c
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2023-10-04 15:59:07 +0400

    fff

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,9 @@
-let navbar = ["HOME", "ABOUT US", "PRODUCT", "CONTACT"]
-let navlink = ["index.html","index.html","index.html","index.html"]
+let navbar = ["HOME", "PRODUCT", "ABOUT US", "PRODUCT", "CONTACT"]
+let navlink = ["index.html", "index.html", "#aboutus","index.html","index.html"]
 let navtext = "<ul class="+"nav_ul"+">";
 let navLength = navbar.length;
 for (let i=0; i<navLength;i++){
-    navtext+="<li class=" + "nav_ul_li" + "><a class=" + "nav_a" + " href="+navlink[i]+">" +navbar[i]+'</a></li>';
+    navtext+="<li onclick=openNav() class=" + "nav_ul_li" + "><a class=" + "nav_a" + " href="+navlink[i]+">" +navbar[i]+'</a></li>';
     console.log(navtext);
 }
 
